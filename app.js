97f0a43100b11ea37fc84a06c59ef6aec7f9defb
chrome289 <sidat289@gmail.com>
2015-02-20 20:31:28 +0530

    Database and PMing added

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require('express')
 var url = require("url");
 var app = express()
-
+var mysql = require('mysql');
+ 
+var connection = mysql.createConnection(
+    {
+      host     : 'localhost',
+      user     : 'root',
+      password : 'root',
+      database : 'chat',
+    }
+);
+connection.connect();
 // respond with "Hello World!" on the homepage
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -31,18 +41,105 @@ var server = app.listen(80, function () {
 
 })
 var io = require('socket.io').listen(server);
+
+var clients =[];      
 io.sockets.on('connection', function (socket) 
 {
-	console.log("user connected 	+"+socket.id)
-	socket.emit('message', { message: socket.id });
+	socket.on('storeinfo', function (data) 
+	{
+		console.log("User "+socket.id+' Connected')
+		var already_exists=0
+		for( var i=0, len=clients.length; i<len; ++i )
+		{
+            var c = clients[i];
+            if(c.clientId == socket.id)
+            {
+                already_exists=1;
+                break;
+            }
+        }
+        if(already_exists==0)
+        {
+        	var clientInfo = new Object();
+	        clientInfo.customId = data;
+	        clientInfo.clientId = socket.id;
+	        clients.push(clientInfo);
+	        console.log("User "+data+' Connected')
+	        io.sockets.emit('messaged', 'User '+data+' Connected');
+    	}
+    });
+
+	//io.sockets.emit('message', '');
+	//socket.emit('message', 'User '+socket.id+' Connected');
     socket.on('disconnect', function()
     {
-        console.log('user disconnected  '+socket.id);
-        socket.emit('message', { message: 'fuck off' });
+        
+    	for( var i=0, len=clients.length; i<len; ++i )
+    	{
+            var c = clients[i];
+            if(c.clientId == socket.id)
+            {
+                console.log('User '+c.customId+' Disonnected');
+        		io.sockets.emit('messaged', 'User '+c.customId+' Disonnected');
+        		clients.splice(i,1);
+                break;
+            }
+        }
     });
-    socket.on('takethis', function(data,moredata)
+
+    socket.on('takethis', function(data,username)
     {
-        console.log('taken '+moredata);
-        socket.emit('message', { message: data ,moremessage: moredata});
+		for( var i=0, len=clients.length; i<len; ++i )
+		{
+            var c = clients[i];
+            if(c.customId == username)
+            {
+                io.sockets.to(c.clientId).emit('messaged', 'User '+username+' says '+data);
+                console.log("Sending "+c.clientId+" a message");
+            }
+        }
+		
+        console.log('taken '+data);
+        //socket.emit('message', 'Recieved '+data);
+    });
+
+    socket.on('login', function(username,password)
+    {
+    	console.log(username);
+ 
+		var queryString = "SELECT * FROM user where username = \""+username+"\"";
+		 
+		connection.query( queryString, function(err, rows,fields){
+		  	if(err)	
+		  	{
+		  		console.log("User doesn't exist");
+		  	}else
+		  	{
+				var obj = rows[0].password;
+				console.log(password);
+				if(obj==password)
+					socket.emit('login', 'User '+username+' is logged in');
+				else
+					socket.emit('wlogin', 'Wrong Password');
+		  	}
+		  });
+    });
+
+	socket.on('displayfriends', function(username)
+    {
+    	var queryString = "select friend2 from friends where friend1 = \""+username+"\"";
+		 console.log("You have friends "+username);
+		connection.query( queryString, function(err, rows,fields){
+		  	if(err)	
+		  	{
+		  		console.log("You have no friends");
+		  	}
+		  	else
+		  	{
+		  		console.log(rows);
+				for(var i in rows)
+					socket.emit('addfriend',rows[i].friend2)
+		  	}
+		  });
     });
 });
