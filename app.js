cb7efd763a3d89cfcbac77f669cfa3606a655fcd
chrome289 <sidat289@gmail.com>
2015-02-23 19:45:43 +0530

    Removed manual login

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ io.sockets.on('connection', function (socket)
 		for( var i=0, len=clients.length; i<len; ++i )
 		{
             var c = clients[i];
-            if(c.clientId == socket.id)
+            if(c.customId == username)
             {
                 already_exists=1;
                 break;
@@ -66,33 +66,34 @@ io.sockets.on('connection', function (socket)
 	        clients.push(clientInfo);
 	        console.log("User "+username+' Connected')
 	        io.sockets.emit('message', 'User '+username+' Connected');
-    	}
-    	var queryString = "select username from user where username = \""+username+"\"";
-		 
-		connection.query( queryString, function(err, rows,fields){
-		  	if(err)	
-		  	{
-		  		console.log("User doesn't exist");
-		  	}
-		  	else
-		  	{
-		  		for(var i in rows)
-		  			i=1
-		  		if(i!=1)
-		  		{
-		  			var queryString = "insert into user values(null,\""+username+"\",\"google+\")";
-					connection.query( queryString, function(err, rows,fields){
-				  	if(err)	
-				  	{
-				  		console.log("User doesn't exist");
-				  	}else
-				  	{
-
-				  	}
-				  	});
-				}
-		  	}
-		});
+    	
+	    	var queryString = "select username from user where username = \""+username+"\"";
+			 
+			connection.query( queryString, function(err, rows,fields){
+			  	if(err)	
+			  	{console.log("User doesn't exist");}
+			  	else
+			  	{
+			  		for(var i in rows)
+			  			i=1;
+			  		if(i!=1)
+			  		{
+			  			var queryString2 = "insert into user values(null,\""+username+"\",\"google+\")";
+						connection.query( queryString2, function(err, rows,fields){
+					  	if(err)	
+					  	{console.log("User doesn't exist");}
+					  	else
+					  	{}
+					  	});
+					}
+			  	}
+			});
+			socket.emit('login','User '+username+' is logged in');
+		}
+		else
+		{
+			socket.emit('wlogin','You are logged in from some other device');
+		}
     });
 
 	//io.sockets.emit('message', '');
@@ -121,12 +122,9 @@ io.sockets.on('connection', function (socket)
 		 
 		connection.query( queryString, function(err, rows,fields){
 		  	if(err)	
-		  	{
-		  		console.log("User doesn't exist");
-		  	}else
-		  	{
-		  		socket.emit('sent',data)
-		  	}
+		  	{socket.emit('notsent','')}
+		    else
+		  	{socket.emit('sent',data)}
 		  });
         console.log('taken '+data);
         //socket.emit('message', 'Recieved '+data);
@@ -140,9 +138,8 @@ io.sockets.on('connection', function (socket)
 		 
 		connection.query( queryString, function(err, rows,fields){
 		  	if(err)	
-		  	{
-		  		console.log("User doesn't exist");
-		  	}else
+		  	{console.log("User doesn't exist");}
+		    else
 		  	{
 				var obj = rows[0].password;
 				console.log(password);
@@ -160,9 +157,7 @@ io.sockets.on('connection', function (socket)
 		 console.log("You have friends "+username);
 		connection.query( queryString, function(err, rows,fields){
 		  	if(err)	
-		  	{
-		  		console.log("You have no friends");
-		  	}
+		  	{console.log("You have no friends");}
 		  	else
 		  	{
 		  		console.log(rows);
@@ -177,14 +172,12 @@ io.sockets.on('connection', function (socket)
 		 console.log("unrecieved messages ");
 		connection.query( queryString, function(err, rows,fields){
 		  	if(err)	
-		  	{
-		  		console.log("You have no friends");
-		  	}
+		  	{console.log("You have no friends");}
 		  	else
 		  	{
 		  		console.log(rows);
 				for(var i in rows)
-					socket.emit('messaged',sendto+'  :  '+rows[i].message)
+					socket.emit('messaged',rows[i].message)
 				var queryString = "update chat set delivered = 1 where friend1 = \""+sendto+"\" and friend2 = \""+username+"\"";
 				console.log("recieved");
 				connection.query( queryString, function(err, rows,fields){
@@ -206,16 +199,14 @@ io.sockets.on('connection', function (socket)
 		 console.log("recieved messages ");
 		connection.query( queryString, function(err, rows,fields){
 		  	if(err)	
-		  	{
-		  		console.log("You have no friends");
-		  	}
+		  	{console.log("You have no friends");}
 		  	else
 		  	{
 		  		console.log(rows);
 				for(var i in rows)
 				{
 					if(rows[i].friend1==sendto)
-						socket.emit('messaged',sendto+'  :  '+rows[i].message)
+						socket.emit('messaged',rows[i].message)
 					if(rows[i].friend1==username)
 						socket.emit('sent',rows[i].message)
 				}
@@ -223,5 +214,33 @@ io.sockets.on('connection', function (socket)
 			}
 		  });
     });
-
+    socket.on('checkuserexists', function(username)
+    {
+    	var queryString = "select * from user where username = \""+username+"\" ";
+		console.log("checking if user "+username+" exists");
+		connection.query( queryString, function(err, rows,fields){
+		  	if(err)	
+		  	{console.log("Error");}
+		  	else
+		  	{
+		  		var exists=0
+		  		for(var i in rows)
+					exists=1;
+				if(exists==0)
+				{
+					var queryString = "insert into user values (null,\""+username+"\",\""+password+"\")";
+					console.log("adding user "+username);
+					connection.query( queryString, function(err, rows,fields){
+					  	if(err)	
+					  	{console.log("Error2");}
+					  	else
+					  	{socket.emit('signup','You have been registered',1);}
+					});
+				}
+				else
+				{socket.emit('signup','User already exists',0);}
+				socket.emit('done','');
+			}
+		  });
+    });
 });
